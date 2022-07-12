aa1352796103599f3f60a9441fc37f37084b9f3e
Apexweb <tbuhnukc@gmail.com>
2022-07-12 20:37:16 +0700

    Update imagecompress.sh

--- imagecompress.sh
+++ imagecompress.sh
@@ -5,6 +5,14 @@
 #### run from parrent image directory
 #find . -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /home/backup/imagecompress.sh" \;
 
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+DBOLD='\033[2m'      #  ${DBOLD}    # полу яркий цвет (тёмно-серый, независимо от цвета)
+
+clear
+
+#check identify, svgo, mogrify
+if [[ $(which identify) && $(which mogrify) && $(which svgo) && $(which squoosh-cli) ]]; then
+
 #convert from bmp to jpg
 find . -maxdepth 1 -iname '*bmp' -type f -exec bash -c "mogrify -format jpg {} && chown www-data:www-data {}" \;
 
@@ -14,13 +22,13 @@ find . -maxdepth 1 -iname '*svg' -type f -exec bash -c "svgo {} && chown www-dat
 #webp and avif
 for current in $(find . -maxdepth 1 \( -iname '*.jpeg' -o -iname '*.jpg' -o -iname '*.png' \) -type f); do
 	if [ ! -f $current.webp ]; then
-		echo 'need create webp from '$current
+		echo -en "${LYELLOW}need create ${DBOLD}webp from $current"
 		cp $current $current.webp
 		squoosh-cli --webp '{"quality":85, "method":4,"sns_strength":50,"filter_strength":60,"filter_type":1,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0, "autofilter":0, "partition_limit":0, "alpha_compression":1, "alpha_filtering":1, "alpha_quality":100,"lossless":0}' $current.webp
 		chown www-data:www-data $current.webp
 	fi
 	if [ ! -f $current.avif ]; then
-		echo 'need create avif from '$current
+		echo -en "${LYELLOW}need create ${DBOLD} avif from $current"
 		cp $current $current.avif
 		squoosh-cli --avif '{"speed":2}' $current.avif
 		chown www-data:www-data $current.avif
@@ -46,3 +54,28 @@ for currentpng in $(find . -maxdepth 1 -iname '*.png' -type f); do
 		chown www-data:www-data $currentpng
 	fi
 done
+
+for cf in $(find . -maxdepth 1 \( -iname '*.jpeg' -o -iname '*.jpg' -o -iname '*.png' \) -type f); do
+	#check vs webp, bigger webp must be delete
+	if [[ $(stat -c%s "$cf.webp") -gt $(stat -c%s "$cf") ]]; then
+	# echo "webp is " && stat -c%s "$cf.webp"
+	# echo "$cf is " && stat -c%s "$cf"
+  	echo -en "webp is ${LYELLOW}bigger ${DBOLD}then $cf, need delete webp"
+	rm "$cf.webp"
+	fi
+	#check vs avif, bigger avif must be delete
+	if [[ $(stat -c%s "$cf.avif") -gt $(stat -c%s "$cf") ]]; then
+	# echo "avif is " && stat -c%s "$cf.avif"
+	# echo "$cf is " && stat -c%s "$cf"
+  	echo -en "avif is ${LYELLOW}bigger ${DBOLD}then $cf, need delete avif"
+	rm "$cf.avif"
+	fi
+done
+
+else
+echo "need install ImageMagick, SVGO (node js packet), Squoosh-cli (node js packet)"
+fi
+
+tput sgr0
+echo ""
+exit
